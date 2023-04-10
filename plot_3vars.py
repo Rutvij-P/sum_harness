4094381964dc589ed84a130bf137854a8923f4b4
Rutu Patel <patelrutvij2801@gmail.com>
2023-04-09 22:40:44 -0700

    with plots and graphs

--- plot_3vars.py
+++ plot_3vars.py
@@ -4,7 +4,7 @@ E. Wes Bethel, Copyright (C) 2022
 
 October 2022
 
-Description: This code loads a .csv file and creates a 3-variable plot
+Description: This code loads a .csv file and creates a 3-variable plot, and saves it to a file named "myplot.png"
 
 Inputs: the named file "sample_data_3vars.csv"
 
@@ -19,68 +19,51 @@ Assumptions: developed and tested using Python version 3.8.8 on macOS 11.6
 import pandas as pd
 import matplotlib.pyplot as plt
 
+plot_fname = "bandwidth.png"
 
-# MFLOP/s plot
-fname_mflop = "sample_data_mflop.csv"
-df_mflop = pd.read_csv(fname_mflop, comment="#")
+fname = "bandwidth.csv"
+df = pd.read_csv(fname, comment="#")
+print(df)
 
-var_names_mflop = list(df_mflop.columns)
-problem_sizes_mflop = df_mflop[var_names_mflop[0]].values.tolist()
-code1_mflop = df_mflop[var_names_mflop[1]].values.tolist()
-code2_mflop = df_mflop[var_names_mflop[2]].values.tolist()
-code3_mflop = df_mflop[var_names_mflop[3]].values.tolist()
+var_names = list(df.columns)
 
-plt.figure()
-plt.title("MFLOP/s Comparison")
-plt.xlabel("Problem Size")
-plt.ylabel("MFLOP/s")
-plt.plot(problem_sizes_mflop, code1_mflop, "r-o")
-plt.plot(problem_sizes_mflop, code2_mflop, "b-x")
-plt.plot(problem_sizes_mflop, code3_mflop, "g-^")
-plt.legend(var_names_mflop[1:], loc="best")
-plt.grid(axis='both')
+print("var names =", var_names)
 
-# Memory Bandwidth plot
-fname_mem_bw = "sample_data_mem_bw.csv"
-df_mem_bw = pd.read_csv(fname_mem_bw, comment="#")
+# split the df into individual vars
+# assumption: column order - 0=problem size, 1=blas time, 2=basic time
 
-var_names_mem_bw = list(df_mem_bw.columns)
-problem_sizes_mem_bw = df_mem_bw[var_names_mem_bw[0]].values.tolist()
-code1_mem_bw = df_mem_bw[var_names_mem_bw[1]].values.tolist()
-code2_mem_bw = df_mem_bw[var_names_mem_bw[2]].values.tolist()
-code3_mem_bw = df_mem_bw[var_names_mem_bw[3]].values.tolist()
+problem_sizes = df[var_names[0]].values.tolist()
+code1_time = df[var_names[1]].values.tolist()
+code2_time = df[var_names[2]].values.tolist()
+code3_time = df[var_names[3]].values.tolist()
 
 plt.figure()
-plt.title("Memory Bandwidth Comparison")
-plt.xlabel("Problem Size")
-plt.ylabel("% Peak Memory Bandwidth")
-plt.plot(problem_sizes_mem_bw, code1_mem_bw, "r-o")
-plt.plot(problem_sizes_mem_bw, code2_mem_bw, "b-x")
-plt.plot(problem_sizes_mem_bw, code3_mem_bw, "g-^")
-plt.legend(var_names_mem_bw[1:], loc="best")
+
+plt.title("Comparison of 3 Codes")
+
+xlocs = [i for i in range(len(problem_sizes))]
+
+plt.xticks(xlocs, problem_sizes)
+
+plt.plot(code1_time, "r-o")
+plt.plot(code2_time, "b-x")
+plt.plot(code3_time, "g-^")
+
+# plt.xscale("log")
+# plt.yscale("log")
+
+plt.xlabel("Problem Sizes")
+plt.ylabel("Memory Bandwidth (GB/s)")
+
+varNames = [var_names[1], var_names[2], var_names[3]]
+plt.legend(varNames, loc="best")
+
 plt.grid(axis='both')
 
-# Memory Latency plot
-fname_mem_lat = "sample_data_mem_lat.csv"
-df_mem_lat = pd.read_csv(fname_mem_lat, comment="#")
+# save the figure before trying to show the plot
+plt.savefig(plot_fname, dpi=300)
 
-var_names_mem_lat = list(df_mem_lat.columns)
-problem_sizes_mem_lat = df_mem_lat[var_names_mem_lat[0]].values.tolist()
-code1_mem_lat = df_mem_lat[var_names_mem_lat[1]].values.tolist()
-code2_mem_lat = df_mem_lat[var_names_mem_lat[2]].values.tolist()
-code3_mem_lat = df_mem_lat[var_names_mem_lat[3]].values.tolist()
-
-plt.figure()
-plt.title("Memory Latency Comparison")
-plt.xlabel("Problem Size")
-plt.ylabel("Memory Latency (ns)")
-plt.plot(problem_sizes_mem_lat, code1_mem_lat, "r-o")
-plt.plot(problem_sizes_mem_lat, code2_mem_lat, "b-x")
-plt.plot(problem_sizes_mem_lat, code3_mem_lat, "g-^")
-plt.legend(var_names_mem_lat[1:], loc="best")
-plt.grid(axis='both')
 
 plt.show()
-
 
 # EOF
