0702ff1cd3e3a7c0a094a5fb697f63dae52696ab
Rutu Patel <patelrutvij2801@gmail.com>
2023-04-13 21:40:30 -0700

    for cori

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,11 +1,3 @@
-//
-// (C) 2022, E. Wes Bethel
-// benchmark-* harness for running different versions of the sum study
-//    over different problem sizes
-//
-// usage: no command line arguments
-// set problem sizes, block sizes in the code below
-
 #include <algorithm>
 #include <chrono>
 #include <iomanip>
@@ -24,68 +16,50 @@ int main(int argc, char** argv)
 {
    std::cout << std::fixed << std::setprecision(2);
 
-#define MAX_PROBLEM_SIZE 1 << 28  //  256M
-   std::vector<int64_t> problem_sizes{ MAX_PROBLEM_SIZE >> 5, MAX_PROBLEM_SIZE >> 4, MAX_PROBLEM_SIZE >> 3, MAX_PROBLEM_SIZE >> 2, MAX_PROBLEM_SIZE >> 1, MAX_PROBLEM_SIZE};
+   const int64_t MIN_PROBLEM_SIZE = 1 << 20; // 1M
+   const int64_t MAX_PROBLEM_SIZE = 1 << 28; // 256M
+   std::vector<int64_t> problem_sizes{ MIN_PROBLEM_SIZE, MAX_PROBLEM_SIZE };
    
    std::vector<uint64_t> A(MAX_PROBLEM_SIZE);
 
    int64_t t;
    int n_problems = problem_sizes.size();
 
+   int64_t total_num_calls = 0;
+   double total_elapsed_time = 0.0;
+
    /* For each test size */
    for (int64_t n : problem_sizes) 
    {
-      printf("Working on problem size N=%d \n", n);
+      int64_t num_calls = 0;
+      double elapsed_time = 0.0;
+
+      printf("Working on problem size N=%lld \n", n);
 
       // invoke user code to set up the problem
-      // measure time for direct sum method
-        setup(n, &A[0]);
-        auto start_time = high_resolution_clock::now();
+      setup(n, &A[0]);
+
+      // measure time for sum method until at least 30 seconds have elapsed
+      auto start_time = high_resolution_clock::now();
+      while (elapsed_time < 30.0) {
         t = sum(n, &A[0]);
+        num_calls++;
         auto end_time = high_resolution_clock::now();
         auto time_diff = duration_cast<nanoseconds>(end_time - start_time);
-        double direct_sum_elapsed_time = time_diff.count() / 1000000000.0;
+        elapsed_time = time_diff.count() / 1000000000.0;
+      }
 
-        printf(" Direct sum result = %lld \n",t);
-        printf(" Direct sum elapsed time = %f seconds \n", direct_sum_elapsed_time);
-        
-        // measure time for vector sum method
-        setup(n, &A[0]);
-        start_time = high_resolution_clock::now();
-        t = sum(n, &A[0]);
-        end_time = high_resolution_clock::now();
-        time_diff = duration_cast<nanoseconds>(end_time - start_time);
-        double vector_sum_elapsed_time = time_diff.count() / 1000000000.0;
+      total_num_calls += num_calls;
+      total_elapsed_time += elapsed_time;
 
-        printf(" Vector sum result = %lld \n",t);
-        printf(" Vector sum elapsed time = %f seconds \n", vector_sum_elapsed_time);
-      
-         // measure time for indirect sum method
-         setup(n, &A[0]);
-         start_time = high_resolution_clock::now();
-         t = sum(n, &A[0]);
-         end_time = high_resolution_clock::now();
-         time_diff = duration_cast<nanoseconds>(end_time - start_time);
-         double indirect_sum_elapsed_time = time_diff.count() / 1000000000.0;
-
-         printf(" Indirect sum result = %lld \n",t);
-         printf(" Indirect sum elapsed time = %f seconds \n", indirect_sum_elapsed_time);
-
-         double direct_sum_mflops = 2 * n / (direct_sum_elapsed_time * 1e6);
-         double vector_sum_mflops = 2 * n / (vector_sum_elapsed_time * 1e6);
-         double indirect_sum_mflops = 2 * n / (indirect_sum_elapsed_time * 1e6);
-
-         double mem_bandwidth = (8 * n) / (direct_sum_elapsed_time * 1e9);
-         double mem_latency = indirect_sum_elapsed_time / (double)n;
-
-         printf(" Direct sum MFLOP/s = %f\n", direct_sum_mflops);
-         printf(" Vector sum MFLOP/s = %f\n", vector_sum_mflops);
-         printf(" Indirect sum MFLOP/s = %f\n", indirect_sum_mflops);
-         printf(" Memory bandwidth = %f GB/s\n", mem_bandwidth);
-         printf(" Memory latency = %f ns\n", mem_latency * 1e9);
+      printf(" Sum result = %lld \n",t);
+      printf(" Number of calls to sum() function = %lld\n", num_calls);
+      printf(" Elapsed time = %f seconds \n", elapsed_time);
    }
+
+   printf("Total number of calls to sum() function = %lld\n", total_num_calls);
+   printf("Total elapsed time = %f seconds\n", total_elapsed_time);
+   printf("Number of times each problem size was executed: %d\n", 2 * n_problems);
 
    return 0;
 }
-                  
-//EOF
